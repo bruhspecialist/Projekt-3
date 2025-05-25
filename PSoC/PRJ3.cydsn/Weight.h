dcf18a0fcbd497dcd279db3478bc6d055fdd43d2
bruhspecialist <88710083+bruhspecialist@users.noreply.github.com>
2025-05-25 21:43:27 +0200

    gpt simplificering af Weight.h

--- PSoC/PRJ3.cydsn/Weight.h
+++ PSoC/PRJ3.cydsn/Weight.h
@@ -1,90 +1,56 @@
 #pragma once
-
 #include "project.h"
-
 #include "UART.h"
+#include <stdbool.h>
+#include <string.h>
 
-#include "stdbool.h"
-
-// Maksimal afvigelse
 #define WEIGHT_TOLERANCE 20
-
-// Kalibreringsværdier (Med 100g preload)
-#define ADC_M1      903     // @ 0 gram
-#define ADC_M2      1536    // @ 400 gram
-
+#define ADC_M1      903
+#define ADC_M2      1536
 #define M1_WEIGHT   0
 #define M2_WEIGHT   415
 
-enum cupSizes {shot = 1, medium = 2, large = 3};
+enum cupSizes { shot = 1, medium = 2, large = 3 };
 
-// Værdier for glas
 #define SHOT_WEIGHT 46
 #define MEDIUM_WEIGHT 67
 #define LARGE_WEIGHT 89
-
-// Værdier for glas, når de er fyldt helt op (100%)
 #define SHOT_FULL_WEIGHT 91
 #define MEDIUM_FULL_WEIGHT 142
 #define LARGE_FULL_WEIGHT 199
 
+static const uint16_t cup_weights[]      = { 0, SHOT_WEIGHT, MEDIUM_WEIGHT, LARGE_WEIGHT };
+static const uint16_t cup_full_weights[] = { 0, SHOT_FULL_WEIGHT, MEDIUM_FULL_WEIGHT, LARGE_FULL_WEIGHT };
+static const char*    cup_names[]        = { "", "shot", "medium", "large" };
+
 int16_t ADC_zeroOffset = 0;
 
-uint16_t CupWeight(enum cupSizes cupSize) {
-    switch (cupSize) {
-        case (shot) : return SHOT_WEIGHT;
-        case (medium) : return MEDIUM_WEIGHT;
-        case (large) : return LARGE_WEIGHT;
-        default : return 0;
-    }
-}
-
-uint16_t CupFullWeight(enum cupSizes cupSize) {
-    switch (cupSize) {
-        case (shot) : return SHOT_FULL_WEIGHT;
-        case (medium) : return MEDIUM_FULL_WEIGHT;
-        case (large) : return LARGE_FULL_WEIGHT;
-        default : return 0;
-    }
-}
-
-const char* CupSizeToString(enum cupSizes cupSize) {
-    switch (cupSize) {
-        case (shot) : return "shot";
-        case (medium) : return "medium";
-        case (large) : return "large";
-        default : return "\0";
-    }
-}
+uint16_t CupWeight(enum cupSizes cupSize) { return cup_weights[cupSize]; }
+uint16_t CupFullWeight(enum cupSizes cupSize) { return cup_full_weights[cupSize]; }
+const char* CupSizeToString(enum cupSizes cupSize) { return cup_names[cupSize]; }
 
 enum cupSizes StringToCupSize(const char* cupSizeString) {
-    if (strcmp(cupSizeString, "shot") == 0)
-        return shot;
-    else if (strcmp(cupSizeString, "medium") == 0)
-        return medium;
-    else if (strcmp(cupSizeString, "large") == 0)
-        return large;
-    else
-        return 0;
+    for (int i = shot; i <= large; i++) {
+        if (strcmp(cup_names[i], cupSizeString) == 0)
+            return (enum cupSizes)i;
+    }
+    return 0;
 }
 
 uint16_t ConvertToGram(uint16_t adc_count) {
-    int32_t diff = (int32_t)adc_count - ADC_M1; // Relativ forskel
-    uint16_t gramRange = M2_WEIGHT - M1_WEIGHT;
-    uint16_t adcRange = ADC_M2 - ADC_M1;
-    int32_t grams = M1_WEIGHT + ((diff * gramRange) / adcRange); // Konvertering til gram
-    if (grams < 0) grams = 0; // Clamp til 0, hvis negativ værdi
+    int32_t diff = (int32_t)adc_count - ADC_M1;
+    int32_t adcRange = ADC_M2 - ADC_M1;
+    if (adcRange == 0) return 0;
+    int32_t grams = M1_WEIGHT + ((diff * (M2_WEIGHT - M1_WEIGHT)) / adcRange);
+    if (grams < 0) grams = 0;
     return grams;
 }
 
 uint16_t Weight_ADC_Read() {
-    if (WEIGHT_ADC_IsEndConversion(WEIGHT_ADC_WAIT_FOR_RESULT)) { // OBS. Kan hænge her
-        UART_USB_PutString("ADC_zeroOffset: ");
-        UART_USB_PutInt(ADC_zeroOffset);
-        UART_USB_PutString("\r\n");
+    if (WEIGHT_ADC_IsEndConversion(WEIGHT_ADC_WAIT_FOR_RESULT))
         return WEIGHT_ADC_GetResult16() - ADC_zeroOffset;
-    }
-    else return 0; // Fejl?
+    else
+        return 0;
 }
 
 uint16_t Weight_Read() {
@@ -98,29 +64,17 @@ uint16_t Weight_Read() {
 
 bool Weight_ValidateCupSize(enum cupSizes cupSize) {
     uint16_t weight = Weight_Read();
-    if (
-        (weight >= CupWeight(cupSize) - WEIGHT_TOLERANCE)
-        && (weight <= CupWeight(cupSize) + WEIGHT_TOLERANCE)
-    ) {
-        return true;
-    }
-    else return false;
+    uint16_t target = CupWeight(cupSize);
+    return (weight >= target - WEIGHT_TOLERANCE) && (weight <= target + WEIGHT_TOLERANCE);
 }
 
 bool Weight_IsCupFull(enum cupSizes cupSize) {
     uint16_t weight = Weight_Read();
-    if (
-        (weight >= CupFullWeight(cupSize) - WEIGHT_TOLERANCE)
-        && (weight <= CupFullWeight(cupSize) + WEIGHT_TOLERANCE)
-    ) {
-        return true;
-    }
-    else return false;
+    uint16_t target = CupFullWeight(cupSize);
+    return (weight >= target - WEIGHT_TOLERANCE) && (weight <= target + WEIGHT_TOLERANCE);
 }
 
-void Weight_Tare() {
-    ADC_zeroOffset = Weight_ADC_Read() - ADC_M1 ;
-}
+void Weight_Tare() { ADC_zeroOffset = Weight_ADC_Read() - ADC_M1; }
 
 void Weight_Initialize() {
     WEIGHT_ADC_Start();
